1e5fe643844e283cee73e519a6d2b2264e4b8e97
ShineBulate <braveshine36@yandex.ru>
2022-08-19 16:59:08 +0800

    alpha ver 0.1

---
+++ function.php
@@ -0,0 +1,85 @@
+<?php
+class User{
+	private $login;
+	private $password;
+	private $re_password;
+	private $name;
+	private $e_mail;
+	private $cookie;
+
+	public function setInfo(){
+		$this->login = $_POST['login'];
+		$this->name = $_POST['name'];
+		$this->e_mail = $_POST['e_mail'];
+	}
+	public function hashing(){
+		$this->password = md5($_POST['user_pass']);
+		$this->re_password = md5($_POST['re_pass']);
+		if($this->password != $this->re_password){
+			echo("<a href='http://localhost/index.php'>Назад</a>");
+			echo("<script>alert('Error password!')</script>");	
+				
+			
+			
+		}
+		else{
+			$this->cookie = setcookie("user_name",$this->name);
+			header("Location:http://localhost/login.php");
+			
+		}
+	}
+		public function isCookie(){
+	
+		if(isset($this->cookie)){
+			
+			echo("Welcome ".$this->name);
+			unset($this->cookie);
+		}
+		else{
+			header("https://github.com/ShineBulate?tab=repositories");
+		
+		}
+		
+	}
+	public function getInfo(){
+		return $this->login."<br>".$this->password."<br>".$this->re_password."<br>".$this->name;
+	}
+	
+
+	
+}
+class DB extends User{
+	function dbconnect(){
+$dbhost = '';
+$dbuser = 'root'; 
+$dbpass = ''; 
+$dbname = 'profiles'; 
+
+$db = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+
+
+if ($db->connect_errno)
+{
+	exit('Error MYSQLI: ' . $db->connect_error);
+}
+		$sql = "INSERT INTO `user` (`login`,`email`,`name`,`password`) VALUES ('$this->login','$this->e_mail','$this->name','$this->password')";
+		if($db->query($sql)){
+    echo "Вы успешно зарегистрированы!";
+} else{
+    echo "Ошибка: " . $db->error;
+}
+$db->close();
+	}
+
+	
+}
+$db = new DB();
+$db->dbconnect();
+$users = new User();
+$users->setInfo();
+$users->hashing();
+$users->isCookie();
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+/*
+if(isset($_COOKIE['user_name'])){
+	header('Location:https://github.com/ShineBulate?tab=repositories');
+}*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,400;0,500;1,100&display=swap" rel="stylesheet">
+    <title>Регистрация</title>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.7.0/dist/vue.js"></script>
+
+</head>
+<header>
+
+</header>
+<body>
+<main>
+<form action="function.php" method="post" name="reg_form" required/><br>
+<input type="text" name="login" placeholder="Логин" required /><br>
+<input type="text" name="name" placeholder="Имя" required /><br>
+<input type="email" name="e_mail" placeholder="E-mail" required /><br>
+<input type="password" name="user_pass" placeholder="Пароль" required / > <br>
+<input type="password" name="re_pass" placeholder="Повторите пароль" required /><br>
+
+<button type="submit">Зарегистрироваться</button>
+</form>
+</main>
+<script>
+function check(){	
+let password = document.getElementsByName('user_pass');
+let re_password = document.getElementsByName('re_password');
+if(password !== re_password ){
+alert('Пароли не совпадают!' + password + re_password);
+
+}
+else{
+	
+}
+}
+</script>
+</body>
+</html>
